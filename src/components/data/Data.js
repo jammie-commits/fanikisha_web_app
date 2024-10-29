4f55d146e478fbf017b7c4e27b5399afb46361ee
CISSEJACKA <jackacisse@gmail.com>
2024-10-29 18:26:22 +0300

    hot fix

--- src/components/data/Data.js
+++ src/components/data/Data.js
@@ -290,14 +290,14 @@ export const price = [
 export const footer = [
   {
     title: "LAYOUTS",
-    text: [{ list: "Home Page" }, { list: "About Page" }, { list: "Service Page" }, { list: "Property Page" }, { list: "Contact Page" }, { list: "Single Blog" }],
+    text: [{ list: "Home Page" }, { list: "About Page" }, { list: "Service Page" }, { list: "Property Page" }, { list: "Contact Page" } ],
   },
   {
     title: "ALL SECTIONS",
-    text: [{ list: "Headers" }, { list: "Features" }, { list: "Attractive" }, { list: "Testimonials" }, { list: "Videos" }, { list: "Footers" }],
+    text: [{ list: "Headers" }, { list: "Testimonials" }, { list: "Videos" }, { list: "Footers" }],
   },
   {
     title: "COMPANY",
-    text: [{ list: "About" }, { list: "Blog" }, { list: "Pricing" }, { list: "Affiliate" }, { list: "Login" }, { list: "Changelog" }],
+    text: [{ list: "About" }, { list: "Blog" },  { list: "Login" }],
   },
 ]
