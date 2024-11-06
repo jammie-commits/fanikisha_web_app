b27db20f76c0e11df00375a7c2495da60de32fd1
jammie-commits <jay.mbugua.ph@gmail.com>
2024-11-06 08:02:11 +0300

    hot fix

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -1,34 +1,52 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Heading from "../../common/Heading"
 import "./hero.css"
+import { featured } from "../../data/Data"
 
 const Hero = () => {
+  const [currentSlide, setCurrentSlide] = useState(0)
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentSlide((prevSlide) => 
+        prevSlide === featured.length - 1 ? 0 : prevSlide + 1
+      )
+    }, 5000) // Changes slide every 5 seconds
+
+    return () => clearInterval(timer)
+  }, [])
+
   return (
     <>
       <section className='hero'>
-        <div className='container'>
-          <Heading title='Welcome To FANIKISHA VENTURES ltd ' subtitle='Bringing you an Executive, Credible, Trustworthy, Secure and reliable Land Investment deals.' />
-
-          {/* <form className='flex'>
-            <div className='box'>
-              <span>City/Street</span>
-              <input type='text' placeholder='Location' />
+        <div className='slider-container'>
+          {featured.map((slide, index) => (
+            <div 
+              key={index}
+              className={`slide ${index === currentSlide ? 'active' : ''}`}
+              style={{
+                backgroundImage: `url(${slide.cover})`,
+              }}
+            >
+              <div className='container'>
+                <div className="hero-heading-wrapper">
+                  <Heading 
+                    title='Welcome To FANIKISHA VENTURES ltd ' 
+                    subtitle='Bringing you an Executive, Credible, Trustworthy, Secure and reliable Land Investment deals.' 
+                  />
+                </div>
+              </div>
             </div>
-            <div className='box'>
-              <span>Property Type</span>
-              <input type='text' placeholder='Property Type' />
-            </div>
-            <div className='box'>
-              <span>Price Range</span>
-              <input type='text' placeholder='Price Range' />
-            </div>
-            <div className='box'>
-              <h4>Advance Filter</h4>
-            </div>
-            <button className='btn1'>
-              <i className='fa fa-search'></i>
-            </button>
-          </form> */}
+          ))}
+          <div className="slider-dots">
+            {featured.map((_, index) => (
+              <span 
+                key={index} 
+                className={`dot ${index === currentSlide ? 'active' : ''}`}
+                onClick={() => setCurrentSlide(index)}
+              />
+            ))}
+          </div>
         </div>
       </section>
     </>
